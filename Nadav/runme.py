bc6eab951c13b131aa455a05df34d2fe37276160
SofferLior <soffer.lior@gmail.com>
2022-06-10 15:43:40 +0300

    remove Nadav code

--- Nadav/runme.py
+++
@@ -1,9 +0,0 @@
-import os
-from gui import ProjectGui
-
-if not os.path.isdir(os.path.join('..', 'Output')):
-    os.makedirs(os.path.join('..', 'Output'))
-
-project_gui = ProjectGui()
-project_gui.loop_gui()
-
